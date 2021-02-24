620521a481fa3cf24c0c121820a46e982e73e4e6
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-02-24 12:49:03 +0100

    conf-env example (please complete yourself) and async

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@ export NAMESPACE=${NAMESPACE:-staging-1-3}
 
 function create-secret() {
     kubectl -n staging-1-3 delete secret dispatcher-conf
-    kubectl -n staging-1-3 create secret generic dispatcher-conf --from-file=conf_env.yml=dispatcher/conf/conf_env.yml
+    kubectl -n staging-1-3 create secret generic dispatcher-conf --from-file=conf_env.yml=conf/conf_env.yml
 }
 
 function install() {
