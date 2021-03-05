221971731baa7a24b0b9c8774fac125cce56cb30
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-03-05 19:48:40 +0100

    add flower

--- make.sh
+++ make.sh
@@ -1,8 +1,10 @@
+export ODA_ENVIRONMENT=${ODA_ENVIRONMENT:-}
 export NAMESPACE=${NAMESPACE:-staging-1-3}
 
+
 function create-secret() {
-    kubectl -n staging-1-3 delete secret dispatcher-conf
-    kubectl -n staging-1-3 create secret generic dispatcher-conf --from-file=conf_env.yml=conf/conf_env.yml
+    kubectl -n $NAMESPACE delete secret dispatcher-conf
+    kubectl -n $NAMESPACE create secret generic dispatcher-conf --from-file=conf_env.yml=conf/conf_env.yml
 }
 
 function install() {
@@ -11,7 +13,7 @@ function install() {
 
 function upgrade() {
     set -x
-    helm upgrade --install -n ${NAMESPACE:?} oda-dispatcher . --set image.tag="$(cd dispatcher; git describe --always)" 
+    helm upgrade --install -n ${NAMESPACE:?} oda-dispatcher . -f values-${ODA_ENVIRONMENT:?}.yaml --set image.tag="$(cd dispatcher; git describe --always)" 
 }
 
 $@
