2bd99fcd00d83415086b3af6cbe471c075fb54fc
Denys Savchenko <speleoden@gmail.com>
2021-03-28 16:32:48 +0200

    NAMESPACE->ODA_NAMESPACE

--- make.sh
+++ make.sh
@@ -1,10 +1,10 @@
 export ODA_ENVIRONMENT=${ODA_ENVIRONMENT:-}
-export NAMESPACE=${NAMESPACE:-staging-1-3}
+export ODA_NAMESPACE=${ODA_NAMESPACE:-oda-staging}
 
 
 function create-secret() {
-    kubectl -n $NAMESPACE delete secret dispatcher-conf || echo ok
-    kubectl -n $NAMESPACE create secret generic dispatcher-conf --from-file=conf_env.yml=conf/conf_env.yml
+    kubectl -n $ODA_NAMESPACE delete secret dispatcher-conf || echo ok
+    kubectl -n $ODA_NAMESPACE create secret generic dispatcher-conf --from-file=conf_env.yml=conf/conf_env.yml
 }
 
 function install() {
@@ -13,7 +13,7 @@ function install() {
 
 function upgrade() {
     set -x
-    helm upgrade --install -n ${NAMESPACE:?} oda-dispatcher . -f values-${ODA_ENVIRONMENT:?}.yaml --set image.tag="$(cd dispatcher; git describe --always)" 
+    helm upgrade --install -n ${ODA_NAMESPACE:?} oda-dispatcher . -f values-${ODA_ENVIRONMENT:?}.yaml --set image.tag="$(cd dispatcher; git describe --always)" 
 }
 
 $@
